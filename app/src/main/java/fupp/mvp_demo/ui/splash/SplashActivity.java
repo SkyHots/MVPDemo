2e3220ce363bf5e51801c2373dbc166c7b8dde60
blankfu <fupeng_2011@foxmail.com>
2019-03-12 16:29:29 +0800

    todo 主界面

--- app/src/main/java/fupp/mvp_demo/ui/splash/SplashActivity.java
+++ app/src/main/java/fupp/mvp_demo/ui/splash/SplashActivity.java
@@ -1,6 +1,5 @@
 package fupp.mvp_demo.ui.splash;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -13,12 +12,9 @@ import fupp.mvp_demo.R;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private Context mContext;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext = this;
         if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {//解决安装器安装app多次启动bug
             finish();
             return;
@@ -33,14 +29,8 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void startLogin() {
-        /*if (SpUtil.prefs.getBoolean("guide", true)) {//安装之后第一次打开App
-            SpUtil.prefs.edit().putBoolean("guide", false).apply();
-            startActivity(new Intent(mContext, GuideActivity.class));
-        } else {//不是第一次打开
-            startActivity(new Intent(mContext, LoginActivity.class));
-        }*/
-        //        startActivity(new Intent(mContext, LoginActivity.class));
-        //        finish();
+        // TODO: 2019/3/12 0012 to main activity
+
     }
 
     public void initView() {
